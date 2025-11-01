b502fe2742445d076e96e202017c31d8be5a3361
bee9827 <ehfrhfo9494@naver.com>
2025-11-01 18:40:01 +0900

    test: Lotto 테스트 추가
    matchCount, contains 함수에 대한 테스트추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,32 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class LottoTest {
+    private static final Lotto DEFAULT_LOTTO = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+    public static Stream<Arguments> matchCount() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 45), 5),
+                Arguments.of(List.of(1, 2, 3, 4, 44, 45), 4),
+                Arguments.of(List.of(1, 2, 3, 43, 44, 45), 3),
+                Arguments.of(List.of(1, 2, 42, 43, 44, 45), 2),
+                Arguments.of(List.of(1, 41, 42, 43, 44, 45), 1),
+                Arguments.of(List.of(40, 41, 42, 43, 44, 45), 0)
+        );
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +40,28 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("matchCount() : 동일한 숫자의 개수를 리턴한다.")
+    @ParameterizedTest
+    @MethodSource
+    void matchCount(List<Integer> numbers, int expectedMatchCount) {
+        Lotto other = new Lotto(numbers);
+
+        int matchCount = DEFAULT_LOTTO.matchCount(other);
+
+        assertThat(matchCount).isEqualTo(expectedMatchCount);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,true",
+            "6,true",
+            "10,false",
+            "45,false",
+    })
+    void contains(int number, boolean expected) {
+        LottoNumber lottoNumber = new LottoNumber(number);
+        boolean contains = DEFAULT_LOTTO.contains(lottoNumber);
+
+        assertThat(contains).isEqualTo(expected);
+    }
 }

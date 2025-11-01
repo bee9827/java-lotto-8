942ed68cb023818314b93ef166b5a5b84056aa9f
bee9827 <ehfrhfo9494@naver.com>
2025-11-01 17:28:55 +0900

    test: LottoNumber test추가
    생성자 및 함수에 대한 테스트 추가.

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -3,7 +3,6 @@ package lotto;
 public enum ErrorCode {
     INVALID_LOTTO_NUMBER("로또 번호는 %d~%d 값 이어야 합니다. 현재 값 : %d", 3),
 
-
     INVALID_ARGS("발생한 에러: %s, 필요한 가변 인자는 %d개 입니다. 현재 값 : %d", 3);
 
     private final String message;
@@ -20,5 +19,4 @@ public enum ErrorCode {
         }
         return String.format(message, args);
     }
-
 }
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,60 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoNumberTest {
+    @Nested
+    @DisplayName("new() : ")
+    class constructor {
+        @DisplayName("int 타입 기본 생성에 성공한다.")
+        @ParameterizedTest
+        @CsvSource(value = {
+                "1",
+                "2",
+                "44",
+                "45",
+        })
+        void intConstructor(int value) {
+            LottoNumber lottoNumber = new LottoNumber(value);
+
+            assertThat(lottoNumber).isEqualTo(value);
+        }
+
+        @DisplayName("[예외] 범위 밖의 값이면 IllegalArgument를 반환한다.")
+        @ParameterizedTest
+        @CsvSource({
+                "0",
+                "-1",
+                "46"
+        })
+        void invalidRange(int value) {
+            assertThatThrownBy(() -> new LottoNumber(value))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(String.valueOf(value));
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,1,true",
+            "2,1,false",
+            "1,2,false"
+    })
+    void isMatch(int LottoNumber, int otherLottoNumber, boolean expected) {
+        //given
+        LottoNumber first = new LottoNumber(LottoNumber);
+        LottoNumber second = new LottoNumber(otherLottoNumber);
+
+        //when
+        boolean match = first.isMatch(second);
+
+        //then
+        assertThat(match).isEqualTo(expected);
+    }
+}

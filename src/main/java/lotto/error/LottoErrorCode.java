600f65262ad9e3676f6e7f1def92fd58f44b0ba3
bee9827 <ehfrhfo9494@naver.com>
2025-11-02 22:52:48 +0900

    feat: WinningLotto 보너스번호 중복 검사
    로또 번호와 중복되는 보너스 번호를 검증한다.

--- src/main/java/lotto/error/LottoErrorCode.java
+++ src/main/java/lotto/error/LottoErrorCode.java
@@ -8,6 +8,7 @@ public enum LottoErrorCode {
     NUMBER_RANGE("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.".formatted(LottoNumber.MIN_VALUE, LottoNumber.MAX_VALUE)),
     NUMBERS_SIZE("로또 번호는 %d개여야 합니다.".formatted(Lotto.SIZE)),
     NUMBERS_DUPLICATED("로또 번호는 중복될 수 없습니다."),
+    BONUS_NUMBER_DUPLICATED("보너스 번호는 로또 번호와 중복될 수 없습니다."),
     MONEY_UNIT("로또 돈은 %d 단위로 입력해 주세요".formatted(LottoMoney.UNIT)),
     MONEY_NOT_USED("사용된 돈이 없습니다."),
     NUMBER_FORMAT("숫자를 입력해 주세요"),
--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,13 +1,22 @@
 package lotto.model;
 
 import java.util.List;
+import lotto.error.LottoErrorCode;
 
 public class WinningLotto extends Lotto {
     private final LottoNumber bonusNumber;
 
     public WinningLotto(List<Integer> numbers, Integer bonusNumber) {
         super(numbers);
+        validateBonusNumberDuplicate(bonusNumber);
         this.bonusNumber = new LottoNumber(bonusNumber);
+    }
+
+    private void validateBonusNumberDuplicate(Integer bonusNumber) {
+        LottoNumber lottoNumber = new LottoNumber(bonusNumber);
+        if (super.contains(lottoNumber)) {
+            throw new IllegalArgumentException(LottoErrorCode.BONUS_NUMBER_DUPLICATED.getMessage());
+        }
     }
 
     public WinningResult matching(Lotto lotto) {

4884e8476038e992d1ed6da8b2be901d834335d0
bee9827 <ehfrhfo9494@naver.com>
2025-11-02 22:52:07 +0900

    refactor: LottoMoney 매직 넘버 제거
    각 의미를 가지는 상수로 변경

--- src/main/java/lotto/model/LottoMoney.java
+++ src/main/java/lotto/model/LottoMoney.java
@@ -5,30 +5,37 @@ import lotto.error.LottoErrorCode;
 public class LottoMoney {
     public static final int UNIT = 1000;
     public static final double PERCENTAGE = 100;
+    private static final int DEFAULT_USED_MONEY = 0;
+    private static final int REMAINING = 0;
+
     private long remainedMoney;
     private long usedMoney;
 
     public LottoMoney(long rest) {
         validateUnit(rest);
         this.remainedMoney = rest;
-        usedMoney = 0;
+        usedMoney = DEFAULT_USED_MONEY;
     }
 
     private void validateUnit(long money) {
-        if (money < UNIT || money % UNIT != 0) {
+        if (money < UNIT || checkInvalidUnit(money)) {
             throw new IllegalArgumentException(LottoErrorCode.MONEY_UNIT.getMessage());
         }
     }
 
+    private boolean checkInvalidUnit(long money) {
+        return money % UNIT != REMAINING;
+    }
+
     public int purchaseTicket() {
         long usingMoney = remainedMoney;
-        remainedMoney = 0;
+        remainedMoney -= usingMoney;
         usedMoney += usingMoney;
         return (int) usingMoney / UNIT;
     }
 
     public double getRevenueRate(Long revenue) {
-        if (usedMoney == 0) {
+        if (usedMoney == DEFAULT_USED_MONEY) {
             throw new IllegalStateException(LottoErrorCode.MONEY_NOT_USED.getMessage());
         }
         return revenue.doubleValue() / usedMoney * PERCENTAGE;

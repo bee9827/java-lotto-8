b1e26f306a4405c6cafe1782c2c94ad2c864608a
bee9827 <ehfrhfo9494@naver.com>
2025-11-03 16:24:59 +0900

    refactor: View의 책임에 따른 인터페이스 제거
    View는 어디로 무엇을 입출력 할지 책임이 있다고 생각했다.
    하지만 입력과 출력은 System.in/out이 담당한다.
    우리는 System.in/out을 사용하여 어떤 형식의 글자를 전달할지 책임만 지면 된다. (Formatter)
    그리고 그 역할은 쉽게 바뀌지 않으므로 인터페이스를 제거한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,32 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.Scanner;
+import lotto.util.Parser;
 
-public interface InputView {
-    long readPurchaseCost();
+public class InputView {
+    public long readPurchaseCost() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Parser.toLong(Console.readLine());
+    }
 
-    List<Integer> readWinningNumbers();
+    public List<Integer> readWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String values = Console.readLine();
+        Scanner scanner = new Scanner(System.in);
 
-    Integer readBonusNumber();
+        return Parser.split(values).stream()
+                .map(Parser::toInteger)
+                .toList();
+    }
 
-    void close();
+    public Integer readBonusNumber() {
+        System.out.println(System.lineSeparator() + "보너스 번호를 입력해 주세요.");
+        return Parser.toInteger(Console.readLine());
+    }
+
+    public void close() {
+        Console.close();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,12 +4,52 @@ import java.util.List;
 import lotto.controller.dto.LottoDto;
 import lotto.model.WinningResult;
 
-public interface OutputView {
-    void printTickets(List<LottoDto> lottoTickets);
+public class OutputView {
+    public void printTickets(List<LottoDto> lottoTickets) {
+        System.out.println(ticketsFormat(lottoTickets));
+    }
 
-    void printWinningResult(List<WinningResult> results);
+    public void printWinningResult(List<WinningResult> results) {
+        System.out.print(winningResultFormat(results));
+    }
 
-    void printRevenueRate(double revenueRate);
+    public void printRevenueRate(double revenueRate) {
+        System.out.println(revenueRateFormat(revenueRate));
+    }
 
-    void printError(String message);
+    private String ticketsFormat(List<LottoDto> lottoTickets) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("%n%d개를 구매했습니다.%n".formatted(lottoTickets.size()));
+        for (LottoDto lotto : lottoTickets) {
+            sb.append(lotto.numbers().toString())
+                    .append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+    private String winningResultFormat(List<WinningResult> results) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator())
+                .append("당첨 통계").append(System.lineSeparator())
+                .append("---").append(System.lineSeparator());
+        for (WinningResult result : WinningResult.getValues()) {
+            if (result == WinningResult.NONE) {
+                continue;
+            }
+            sb.append("%s (%,d원) - %d개%n"
+                    .formatted(result.getCondition(), result.getPrize(), getCount(results, result)));
+        }
+        return sb.toString();
+    }
+
+    private int getCount(List<WinningResult> results, WinningResult result) {
+        return results.stream()
+                .filter(r -> r == result)
+                .toList()
+                .size();
+    }
+
+    private String revenueRateFormat(double revenueRate) {
+        return String.format("총 수익률은 %.1f%%입니다.", revenueRate);
+    }
 }

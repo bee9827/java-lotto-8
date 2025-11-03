ab6044e0a7871fe4184349a202baf9e65919af44
bee9827 <ehfrhfo9494@naver.com>
2025-11-03 23:23:53 +0900

    refactor: 고정된 Tick출력하는 머신 함수화

--- src/test/java/lotto/model/IntegrationTest.java
+++ src/test/java/lotto/model/IntegrationTest.java
@@ -2,7 +2,6 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -21,6 +20,7 @@ import org.junit.jupiter.api.Test;
 public class IntegrationTest {
 
     private OutputStream output;
+
     @BeforeEach
     void init() throws IOException {
         String fakeInput = "5000\n1,2,3,4,5,6\n7";
@@ -33,13 +33,7 @@ public class IntegrationTest {
 
     @Test
     void test() {
-        LottoMachine fixedLottoMachine = (n) -> {
-            List<Lotto> ret = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                ret.add(new Lotto(LottoTest.getDefaultNumbers()));
-            }
-            return ret;
-        };
+        LottoMachine fixedLottoMachine = getDefaultLottoGenerator();
         LottoController lottoController = new LottoController(new InputView(), new OutputView(), fixedLottoMachine);
 
         lottoController.run();
@@ -57,5 +51,15 @@ public class IntegrationTest {
                         "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
                         "6개 일치 (2,000,000,000원) - 5개",
                         "총 수익률은 200,000,000.0%입니다.");
+    }
+
+    private LottoMachine getDefaultLottoGenerator() {
+        return (n) -> {
+            List<Lotto> ret = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                ret.add(new Lotto(LottoTest.getDefaultNumbers()));
+            }
+            return ret;
+        };
     }
 }

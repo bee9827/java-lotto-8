3e2ac818f7e760e2589bf1852062b1a8787667fe
bee9827 <ehfrhfo9494@naver.com>
2025-12-29 16:41:15 +0900

    feat: 로또 번호를 관리한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,10 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    public static final int NUMBER_LENGTH = 6;
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,23 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+        if (numbersNotInRange(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.");
+        }
+        if (getDistinctCount(numbers) != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean numbersNotInRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number < NUMBER_MIN || number > NUMBER_MAX);
+    }
+
+    private long getDistinctCount(List<Integer> numbers) {
+        return numbers.stream().distinct().count();
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,25 +1,54 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    @DisplayName("new Lotto(): ")
+    @Nested
+    class Constructor {
+        @ParameterizedTest(name = "로또 번호 개수: {0}")
+        @DisplayName("로또_번호의_개수가_6개가_아니면_예외가_발생한다")
+        @CsvSource({
+                "7,1,2,3,4,5,6",
+                "4,1,2,3"}
+        )
+        void 로또_번호의_개수가_6개가_아니면_예외가_발생한다(String numbers) {
+            assertThatThrownBy(() -> new Lotto(getNumbers(numbers)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
+        @ParameterizedTest(name = "예외 값: {0}")
+        @DisplayName("로또 번호가 1~45가 아니라면 예외가 발생한다.")
+        @CsvSource({
+                "0,1,2,3,4,5",
+                "46,1,2,3,4,5"
+        })
+        void numbersInvalidRange(String numbers) {
+            assertThatThrownBy(() -> new Lotto(getNumbers(numbers)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        private List<Integer> getNumbers(String numbers) {
+            return Arrays.stream(numbers.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        }
+
+        @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+        @Test
+        void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+            assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
